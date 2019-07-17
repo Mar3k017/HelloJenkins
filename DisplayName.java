66e0673be50f03df0d0af1f1fef115266c7b4207
Marek Kundziewicz <kundziewiczm@tt.com.pl>
2019-07-17 23:39:21 +0200

    modified DisplayName

--- DisplayName.java
+++ DisplayName.java
@@ -1,6 +1,10 @@
 public class DisplayName {
 	public static void main(String ... args) {
 		System.out.println("Marek");
+		System.out.println("Michał");
+		System.out.println("Mariusz");
+		System.out.println("Marcin");
+		
 	}
 }
 
